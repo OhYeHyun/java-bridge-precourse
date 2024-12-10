9fb1e6376c3cd7d552e3984c2c9ab5132f191a8e
OhYeHyun <ohyhohyl@gmail.com>
2024-12-10 20:40:54 +0900

    refactor(validator): static 메서드로 변경

--- src/main/java/bridge/validator/LengthValidator.java
+++ src/main/java/bridge/validator/LengthValidator.java
@@ -2,19 +2,19 @@ package bridge.validator;
 
 public class LengthValidator {
 
-    public void validate(String input) {
+    public static void validate(String input) {
         validateEmpty(input);
         validateIsNumeric(input);
         validateRange(input);
     }
 
-    private void validateEmpty(String input) {
+    private static void validateEmpty(String input) {
         if (input.isEmpty()) {
             throw new IllegalArgumentException("[ERROR] 입력값이 없습니다.");
         }
     }
 
-    private void validateIsNumeric(String input) {
+    private static void validateIsNumeric(String input) {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
@@ -22,7 +22,7 @@ public class LengthValidator {
         }
     }
 
-    private void validateRange(String input) {
+    private static void validateRange(String input) {
         int number = Integer.parseInt(input);
         if (number < 3 || number > 20) {
             throw new IllegalArgumentException("[ERROR] 3 이상 20 이하의 숫자로 입력해 주세요.");
--- src/main/java/bridge/validator/MoveValidator.java
+++ src/main/java/bridge/validator/MoveValidator.java
@@ -4,19 +4,19 @@ import java.util.Objects;
 
 public class MoveValidator {
 
-    public void validate(String input) {
+    public static void validate(String input) {
         validateEmpty(input);
         validateType(input);
     }
 
-    private void validateEmpty(String input) {
+    private static void validateEmpty(String input) {
         if (input.isEmpty()) {
             throw new IllegalArgumentException("[ERROR] 입력값이 없습니다.");
         }
     }
 
-    private void validateType(String input) {
-        if (!Objects.equals(input, "U") || !Objects.equals(input, "D")) {
+    private static void validateType(String input) {
+        if (!Objects.equals(input, "U") && !Objects.equals(input, "D")) {
             throw new IllegalArgumentException("[ERROR] U 또는 D로 입력해 주세요.");
         }
     }

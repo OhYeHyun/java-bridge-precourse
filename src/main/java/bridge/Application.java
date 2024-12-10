01185f04c311bb732fb3d221025c96c7c7ad9bd7
OhYeHyun <ohyhohyl@gmail.com>
2024-12-10 20:43:57 +0900

    feat(Application): 컨트롤러 실행

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,30 @@
 package bridge;
 
+import bridge.controller.BridgeController;
+import bridge.domain.Bridge;
+import bridge.domain.BridgeGenerator;
+import bridge.service.BridgeGame;
+import bridge.service.CrossService;
+import bridge.service.GameSettings;
+import bridge.service.TryService;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BridgeGenerator generator = new BridgeGenerator();
+        Bridge bridge = Bridge.getInstance();
+        GameSettings settings = new GameSettings(generator, bridge);
+
+        CrossService crossService = new CrossService(bridge);
+        TryService tryService = new TryService();
+        BridgeGame game = new BridgeGame(crossService, tryService);
+
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        BridgeController controller = new BridgeController(settings, game, inputView, outputView);
+        controller.run();
     }
 }
